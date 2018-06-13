9b590eae33d65fdab4796615ed76580b65468969
gayatri_soni <gayatrisoni5496@gmail.com>
2018-06-13 15:40:22 +0530

    adding assignment 1

---
+++ assignmnt1.py
@@ -0,0 +1,39 @@
+#q1 print anything you want on screen
+
+print("hey world! what's up")
+
+#q2 join two strings using +  
+
+#method 1
+print("Gayatri"+"soni")
+
+#method 2
+x= "hello"
+y= "world"
+print(x+y)
+
+#q3 take the input of three variables x, y , z print their values on screen.
+
+x= input("enter any value")
+y= input("enter any value")
+z= input("enter any value")
+p=x+y+z
+print(p)
+
+#q4 print let's get started on screen.
+
+print("let's get started")
+
+#q5 print the following values using placeholders s="acadview" , course="python" , fees= 5000
+
+s="acadview"
+course="python"
+fees=5000
+print("%s %s %d" %(s , course , fees))
+
+#q6 let's do some exercise name="Tony stark" salary=1000000 print('%s''%d')%(  ,  )
+
+name="Tony stark"
+salary=1000000
+print('%s''%d' %( name , salary))
+
