524dec7cf1c718713674b8632f18a369a51a8fa6
gayatri_soni <gayatrisoni5496@gmail.com>
2018-06-26 14:31:44 +0530

    q6 modified

--- assignment13/q6.py
+++ assignment13/q6.py
@@ -3,19 +3,30 @@
 class AgeTooSmallError(Exception):
 	pass
 	
-try:
-	def ag():                                 #age func is defined
+# try:
+	# def ag():                                 #age func is defined
+		# num=int(input("enter your age"))      #age is input
+		# if num<18:
+			# print("you are too small")
+			# raise AgeTooSmallError("age too small exception")            #exception raised if age less than 18
+			
+		# else:
+			# print("congo")
+	# ag()                               #func is called
+# except AgeTooSmallError as e:
+	# print(e)                          #depicts age too small error is occured
+	# ag()                               #if age less then 18 func called again after raising exception
+# else:
+	# print("no exception")              
+
+	
+while True:
+	try:
 		num=int(input("enter your age"))      #age is input
 		if num<18:
 			print("you are too small")
-			raise AgeTooSmallError("age too small exception")            #exception raised if age less than 18
-			
-		else:
-			print("congo")
-	ag()                               #func is called
-except AgeTooSmallError as e:
-	print(e)                          #depicts age too small error is occured
-	ag()                               #if age less then 18 func called again after raising exception
-else:
-	print("no exception")              
-	
+			raise AgeTooSmallError("age too small exception")
+		break
+	except AgeTooSmallError as e:
+		print(e)
+print("congo")
